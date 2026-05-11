8f7574df60f854e543bd8a29609d4038ccb85d03
Matheus Bertolini <matheusvitor1200@gmail.com>
2026-05-11 19:02:02 -0300

    started js

---
+++ typingspeed.js
@@ -0,0 +1,46 @@
+const startBtn = document.getElementById("startBtn")
+const startupArea = document.querySelector(".challenge-area-startup")
+const challengeTextArea = document.querySelector(".challenge-textarea")
+
+async function handleRandomText(difficulty) {
+    const res = await fetch('./data.json')
+    const data = await res.json()
+
+    let rnumber = Math.floor(Math.random() * data[difficulty].length)
+
+    let challengeText = challengeTextArea.placeholder = data[difficulty][rnumber].text
+
+    console.log(challengeText)
+    return challengeText
+}
+
+function handleDifficulty() {
+    const input = document.querySelector("input[name='difficulty']:checked")
+    return input.id
+}
+
+async function handlePlaceholder(difficulty) {
+    const divPlaceholder = document.querySelector(".fake-placeholder")
+    divPlaceholder.textContent = await handleRandomText(difficulty)
+}
+
+
+function handleStartGame() {
+    startupArea.classList.add("lg-hide")
+    challengeTextArea.classList.remove("blur")
+    challengeTextArea.disabled = false
+    challengeTextArea.textContent = ""
+    challengeTextArea.focus()
+
+    handlePlaceholder(handleDifficulty())
+}
+
+/*"input" > compare the strings
+    if wrong red
+    if right green
+
+
+    the .value[index] works on textarea
+*/
+
+startBtn.addEventListener("click", handleStartGame)
