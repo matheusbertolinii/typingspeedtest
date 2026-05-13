5681fab97ea9e6f9c64e4400667c8f1833047431
Matheus Bertolini <matheusvitor1200@gmail.com>
2026-05-13 00:50:58 -0300

    added color changes to correct letters

--- typingspeed.js
+++ typingspeed.js
@@ -1,17 +1,27 @@
 const startBtn = document.getElementById("startBtn")
 const startupArea = document.querySelector(".challenge-area-startup")
+const challengeArea = document.querySelector(".challenge-area")
 const challengeTextArea = document.querySelector(".challenge-textarea")
 const restartBtn = document.querySelector(".restart-container")
+const spanContainer = document.querySelector(".span-container")
+const fakePlaceholder = document.querySelector(".fake-placeholder")
+
 
 async function handleRandomText(difficulty) {
-    const res = await fetch('./data.json')
-    const data = await res.json()
+    try {
+        const res = await fetch('./data.json')
+        if (!res.ok) {
+            throw new Error('Request Failed')
+        }
+        const data = await res.json()
+        let rnumber = Math.floor(Math.random() * data[difficulty].length)
 
-    let rnumber = Math.floor(Math.random() * data[difficulty].length)
+        let challengeText = data[difficulty][rnumber].text
 
-    let challengeText = challengeTextArea.placeholder = data[difficulty][rnumber].text
-
-    return challengeText
+        return challengeText
+    } catch (err) {
+        console.log("error")
+    }
 }
 
 function handleDifficulty() {
@@ -22,6 +32,68 @@ function handleDifficulty() {
 async function handlePlaceholder(difficulty) {
     const divPlaceholder = document.querySelector(".fake-placeholder")
     divPlaceholder.textContent = await handleRandomText(difficulty)
+    return divPlaceholder.textContent
+}
+
+let input = ''
+
+function backspace(event, index) {
+    if (event.keyCode == 8) {
+        placeholderSpan[index].style.color = "inherit"
+        placeholderSpan[index].classList.remove("actual")
+        placeholderSpan[index + 1].classList.remove("actual")
+    }
+}
+
+async function handleHits() {
+
+    let placeholder = [...await handlePlaceholder(handleDifficulty())]
+    fakePlaceholder.textContent = ''
+    placeholder.forEach(el => {
+        const span = document.createElement("span")
+        span.textContent = el
+        fakePlaceholder.appendChild(span)
+    })
+    const placeholderSpan = document.querySelectorAll(".fake-placeholder span")
+    placeholderSpan[0].classList.add("actual")
+
+    challengeTextArea.addEventListener("input", function changeColor() {
+        input = this.value
+        spanContainer.textContent = ''
+        input.split('').forEach((el, index = 0) => {
+            const span = document.createElement("span")
+            span.textContent = el
+            backspace(event, index)
+            if (index == placeholderSpan.length - 1) {
+                placeholderSpan[index].classList.remove("actual")
+            } else {
+                placeholderSpan[index].classList.remove("actual")
+                placeholderSpan[index + 1].classList.add("actual")
+            }
+
+            if (span.textContent === placeholderSpan[index].textContent) {
+                span.classList.add("correct")
+                placeholderSpan[index].style.color = "transparent"
+            } else if (span.textContent !== placeholderSpan[index].textContent) {
+                span.classList.add("wrong")
+                placeholderSpan[index].style.color = "transparent"
+                span.textContent = placeholderSpan[index].textContent
+            }
+            spanContainer.appendChild(span)
+        })
+    })
+    // challengeTextArea.addEventListener("keydown", backspace(event, index))
+}
+
+challengeArea.addEventListener("click", () => challengeTextArea.focus())
+
+function handleRestart() {
+    challengeTextArea.focus()
+    challengeTextArea.value = ''
+    spanContainer.replaceChildren()
+    fakePlaceholder.replaceChildren()
+
+    handleStartGame()
 }
 
 
@@ -34,6 +106,7 @@ function handleStartGame() {
     restartBtn.classList.remove("hide")
 
     handlePlaceholder(handleDifficulty())
+    handleHits()
 }
 
 /*"input" > compare the strings
@@ -46,4 +119,4 @@ function handleStartGame() {
 */
 
 startBtn.addEventListener("click", handleStartGame)
-restartBtn.addEventListener("click", handleStartGame)
+restartBtn.addEventListener("click", handleRestart)
